a1789fec15948fc0441e81c1965c738a06c1e550
irrusja <irrusja@gmail.com>
2015-02-28 00:57:32 +0200

    Added Genre.php

---
+++ Movies/src/Uek/MovieBundle/Entity/Genre.php
@@ -0,0 +1,64 @@
+<?php
+// src/Uek/MovieBundle/Entity/Movie.php
+namespace Uek\MovieBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * Uek\MovieBundle\Entity\Movie
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="genres")* 
+ *
+ * @ORM\HasLifecycleCallbacks
+ */
+class Genre {
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
+	
+	/**
+	 * @ORM\Column(type="string", nullable=false)
+	 */
+	protected $name;
+	
+	public  function __construct() {
+	}
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Genre
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
